3378d9d8e6b5c25833044ab32cbc273059c7ffd6
unknown <justin.bakoubolo@student.junia.com>
2023-01-10 15:09:53 +0100

    Added comment to bash script

--- connect.sh
+++ connect.sh
@@ -1,19 +1,26 @@
 #! /bin/sh
 
+#getting the  wifi ID and password as terminal argument
+
 if [[ $# != 2 ]]; then
     echo "Usage: $0 wifi_connection_name wifi_conection_password"
     exit 1
 fi
+
+#Checking the output for xenial-finch
 
 if [ -z "$(mdt devices | grep 'xenial-finch')" ]; then
     echo 'Device not found'
     exit 1
 fi
 
+# Printing out argument
 echo "Attempting to connect to Wi-Fi $1"
 
+#Connecting to the wifi using mendel
 mdt exec "nmcli dev wifi connect \"$1\" password \"$2\" ifname wlan0"
 
+#if there is no output print connected to wifi
 if [ "$(mdt exec \"echo \$?\")" == "0" ]; then
     echo "Connected to Wi-Fi $1"
 else
@@ -24,4 +31,5 @@ fi
 echo "Connected to Wi-Fi $1"
 echo "Connecting to shell..."
 
+#Openning up the mendel shell
 mdt shell
